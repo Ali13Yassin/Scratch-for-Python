710f318d70ff793076f7cc947d1a104966671f51
Ali Yassin <30940865+Ali13Yassin@users.noreply.github.com>
2024-01-25 00:56:50 +0200

    Started work on basic playground UI
    and made window size bigger,

--- Main code/Start.py
+++ Main code/Start.py
@@ -23,22 +23,20 @@ def start():
     wind.mainloop() #Makes the window apear
 
 def window():
-    global wind,div,fnt #Defines the main window, and div
+    global wind, div, fnt  # Defines the main window, and div
     wind = Tk()
     wind.configure(bg="#7e8082")  # Change background color to blue
-    centx = int((wind.winfo_screenwidth() - 600) / 2) #Gets coordinates of where to center the window on x-axis
-    centy = int((wind.winfo_screenheight() - 400) / 2) #Gets coordinates of where to center the window on y-axis
-    wind.geometry("600x400+{}+{}".format(centx, centy)) #Centers the window
-    wind.iconbitmap("SFP.ico") #Changes icon
-    # wind.grid_rowconfigure(1, weight=1)     #Helps center objects
-    # wind.grid_columnconfigure(1, weight=1)  #Helps center objects
+    centx = int((wind.winfo_screenwidth() - 1100) / 2) #Gets coordinates of where to center the window on x-axis
+    centy = int((wind.winfo_screenheight() - 650) / 2) #Gets coordinates of where to center the window on y-axis
+    wind.geometry("1100x650+{}+{}".format(centx, centy)) #Centers the window
+    wind.iconbitmap("SFP.ico")  # Changes icon
     wind.title("Project SFP")
-    fnt = "Manrope" #To be able to change font through one change
-    div = Frame(wind) #Funny name from html that's not gonna cause issues at all lolol
+    fnt = "Manrope"  # To be able to change font through one change
+    div = Frame(wind)  # Funny name from HTML that's not gonna cause issues at all lolol
     frame_style = Style()
     frame_style.configure("Custom.TFrame", background="#7e8082")
     div.configure(style="Custom.TFrame")
-    div.grid(row=0, column=0, sticky="nsew") #The frame keeps the layout decent
+    div.place(relx=0, rely=0, relwidth=1, relheight=1)  # The frame keeps the layout decent
     div.grid_rowconfigure(0, weight=1)
     div.grid_columnconfigure(0, weight=1)
 
@@ -63,20 +61,29 @@ def clear():
     for widget in div.winfo_children():
         widget.destroy() #Used this instead of forget to clear memory
 #------------------------------------<Initialization End>-------------------------------------------------------
-#------------------------------------<Editing start>-------------------------------------------------------
+#------------------------------------<Playground start>-------------------------------------------------------
 def playgroundmenu():
     clear()
-    playground = Frame(div) #Funny name from html that's not gonna cause issues at all lolol
+
+    total_width = wind.winfo_width()
+
+    # 70% for playground
+    playground_width = int(total_width * 70)
     frame_style = Style()
     frame_style.configure("playground.TFrame", background="#2e2e2e")
+    playground = Frame(div, style="playground.TFrame")
     playground.configure(style="playground.TFrame")
-    playground.grid(row=0, column=2, padx=10, pady=5, sticky="nsew") #The frame keeps the layout decent
-    sidebar = Frame(div) #Funny name from html that's not gonna cause issues at all lolol
+    playground.place(relx=0, rely=0, relwidth=0.7, relheight=1)
+
+    # 30% for sidebar
+    sidebar_width = int(total_width * 0.3)
     frame_style = Style()
     frame_style.configure("sidebar.TFrame", background="#c4c4c4")
+    sidebar = Frame(div, style="sidebar.TFrame")
     sidebar.configure(style="sidebar.TFrame")
-    sidebar.grid(row=0, column=1, padx=10, pady=5, sticky="nsew") #The frame keeps the layout decent
-    Button(playground, text="playground", command=mainmenu).grid(row=0, column=0, padx=10, pady=5)
-    Button(sidebar, text="sidebar", command=mainmenu).grid(row=0, column=0, padx=10, pady=5)
-#------------------------------------<Editing End>-------------------------------------------------------
+    sidebar.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
+
+    Button(playground, text="playground", command=mainmenu).pack()
+    Button(sidebar, text="sidebar", command=mainmenu).pack()
+#------------------------------------<Playground End>-------------------------------------------------------
 start()
